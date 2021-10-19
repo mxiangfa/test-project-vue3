e819a914bfe6b8481be8ab211b19b105d8997fac
mxiangfa <40046788+mxiangfa@users.noreply.github.com>
2021-10-19 21:45:51 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,53 @@
+/*
+ * @Author: mengxiangfa
+ * @Date: 2021-10-18 16:38:59
+ * @LastEditors: mengxiangfa
+ * @LastEditTime: 2021-10-19 14:07:20
+ * @Description: file content
+ * @FilePath: \test-project\vite.config.js
+ */
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import vue from "@vitejs/plugin-vue"
+function pathResolve(dir) {
+  return resolve(__dirname, ".", dir)
+}
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "/@/": pathResolve("src"),
+    },
+  },
+
+  /**
+   * 在生产中服务时的基本公共路径。
+   * @default '/'
+   */
+  base: "./",
+  optimizeDeps: {
+    include: ["axios"],
+  },
+  build: {
+    target: "modules",
+    outDir: "dist",
+    assetsDir: "assets",
+    minify: "terser", // 混淆器
+  },
+  server: {
+    host: "0.0.0.0",
+    // port: 8080,//端口
+    open: true, // 是否自动在浏览器打开
+    https: false, // 是否开启 https
+    ssr: false, // 服务端渲染
+    // cors: true,
+    // proxy: {
+    //   "/api": {
+    //     target: "http://192.168.99.223:3000", //代理接口
+    //     changeOrigin: true,
+    //     rewrite: path => path.replace(/^\/api/, ""),
+    //   },
+    // },
+  },
+})
